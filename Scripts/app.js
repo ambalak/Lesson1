ef9d0cb67c94778cb1413cb6e2cb120b463e0b3b
ambalak <chandru.vanar@gmail.com>
2020-05-15 14:28:57 -0400

    added additional libs

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,7 +5,21 @@
     {
         console.log("App Started...");
 
+        let firstHeading = document.getElementById("firstHeading");
+        firstHeading.textContent = "good bye hello World!";   
+        firstHeading.style.fontWeight = "bold";
+
+        let main = document.getElementsByTagName("main")[0];
+        let newH2 = document.createElement("h2");
+        newH2.setAttribute("class","Display-5");
+        newH2.textContent= "secondary heading";
+        main.appendChild(newH2);
+
+
+        //document.getElementById("firstHeading").textContent = "Good Day Pa!";
     }
+
+
     window.addEventListener("load",Start);
 
 
