012f20930be9f3c607ef3738dc1f4a6a69741a60
ambalak <chandru.vanar@gmail.com>
2020-05-15 20:24:44 -0400

    second commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,23 @@
+"use strict";
 //IIFE - Immediately Invoked Function Expression
 //Anonymous self executing function
 (function(){
+
+
+    let newH2;
+    //event handlers
+    function MouseOver(){
+        newH2.style.color = "green";
+        newH2.style.fontSize = "80px";
+
+    };
+
+    function MouseOut(){
+        newH2.style.color = "black";
+        newH2.style.fontSize = "30px";
+
+    };
+
     function Start()
     {
         console.log("App Started...");
@@ -10,14 +27,25 @@
         firstHeading.style.fontWeight = "bold";
 
         let main = document.getElementsByTagName("main")[0];
-        let newH2 = document.createElement("h2");
+        newH2 = document.createElement("h2");
         newH2.setAttribute("class","Display-5");
         newH2.textContent= "secondary heading";
         main.appendChild(newH2);
 
 
         //document.getElementById("firstHeading").textContent = "Good Day Pa!";
+
+        newH2.addEventListener('click',function(){
+            newH2.style.color = (newH2.style.color == "blue")? "black" : "blue";
+        });
+
+        newH2.addEventListener('mouseover',MouseOver);
+
+        newH2.addEventListener('mouseout',MouseOut);
+          
     }
+
+    
 
 
     window.addEventListener("load",Start);
